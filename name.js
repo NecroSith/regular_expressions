0a121953c1ba46e611512be4911a0edd966a1cc1
NecroSith <NecroSith@yandex.ru>
2021-01-14 12:05:06 +0300

    Name Regexp updated

--- name.js
+++ name.js
@@ -1,6 +1,10 @@
-const nameExpr = /(?!.*\.\.)(?!.*\s\s)(?!.*,,)[A-Z][A-Za-z\., ]{2,30}/g;
+const nameExpr = /^(?<!.*-)(?!.*\s\s)(?!.*\.\.)[A-Z][A-Za-z\., ]+$/g;
 
-const data = "Samuel L. Jackson\nMike Bay\nMisha Smith\nDr. Keanu Reeves\nYan  Pustynnyy\nDr.. Notoshi\nGangsta2 Dropdown\nTupa- Shakur";
+const data = ["Samuel L. Jackson", "Mike Bay", "Misha Smith", "Dr. Keanu Reeves", "Yan  Pustynnyy", "Dr.. Notoshi", "Gangsta2 Dropdown", "Tupa- Shakur"];
 
-const test = data.match(nameExpr);
+const test = data.map(item => {
+    if (item.match(nameExpr)) {
+        return item.match(nameExpr);
+    }
+}).filter(el => el);
 console.log(test);
