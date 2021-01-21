955d86d90d3f03a1c03184212ecc3256040aa106
NecroSith <NecroSith@yandex.ru>
2021-01-21 18:45:10 +0300

    Updated rules for name regexp

--- name.js
+++ name.js
@@ -1,10 +1,16 @@
+// Rules
+
+// Must begin with capital letter
+// After that there can be either letters of both cases or dot, comma of space
+// Must not contain twoconsecutive spaces or dots
+
 const nameExpr = /^(?<!.*-)(?!.*\s\s)(?!.*\.\.)[A-Z][A-Za-z\., ]+$/g;
 
 const data = ["Samuel L. Jackson", "Mike Bay", "Misha Smith", "Dr. Keanu Reeves", "Yan  Pustynnyy", "Dr.. Notoshi", "Gangsta2 Dropdown", "Tupa- Shakur"];
 
 const test = data.map(item => {
-    if (item.match(nameExpr)) {
-        return item.match(nameExpr);
+    if (item.match(emailExpr)) {
+        return item.match(emailExpr);
     }
 }).filter(el => el);
 console.log(test);
